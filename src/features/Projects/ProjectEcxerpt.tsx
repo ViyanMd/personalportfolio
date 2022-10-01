169f67696740759d7f245448a4b6b3ce8a666bd6
ViyanMd <viyanomd@gmail.com>
2022-10-01 19:48:04 +0200

    beta

--- src/features/Projects/ProjectEcxerpt.tsx
+++ src/features/Projects/ProjectEcxerpt.tsx
@@ -11,9 +11,9 @@ export const ProjectEcxerpt: React.FC<Props> = (props) => {
         <div className="project">
             <img className="decor__coin"src={coin} alt="coin" />
             <div className="project__desc">
-                <a className ="project__desc_name" href={project.repo}>{project.name}</a>
+                <a className ="project__desc_name" href={project.repo} target="_blank" rel="noreferrer">{project.name}</a>
                 <p>{project.description}</p>
-                <a href={project.link}>Live URL</a>
+                <a href={project.link} target="_blank" rel="noreferrer">Live URL</a>
                 <div className="project__stack">Stack: {stack}</div>
             </div>
         </div>
